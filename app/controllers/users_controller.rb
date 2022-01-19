b2b5028dcd6256801def9f1e2a218270ffeca46d
Subhash2807 <lcs2019016@iiitl.ac.in>
2022-01-19 15:06:35 +0530

    profile image and sign up added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+      updated_user_params = user_params
+      @user = User.new(user_params)
+      if @user.save
+        @user.profile = Profile.new
+        flash[:success] = "Signup successfully"
+        redirect_to login_path
+      else
+        flash[:error] = @user.errors.full_messages[0]
+        redirect_to signup_path
+      end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
